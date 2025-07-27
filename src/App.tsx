d423f2d7769f5836eeb8f260a18cb50ce369a335
vladT <q100km@gmail.com>
2025-07-27 13:29:49 +0500

    Настройка проекта + иконки

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+  /* min-height: 100vh; */
+  text-align: center;
+  color: red;
+  background: black;
+`
+
+function App() {
+  //
+  return (
+    <Div>
+      <div>123</div>
+      <i className='fa fa-calendar'></i>
+    </Div>
+  )
+}
+
+export default App
